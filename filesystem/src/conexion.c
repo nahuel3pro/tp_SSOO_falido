a2575c2a814520511dda7003a225c16cd9864021
acorromolas11 <acorromolas11@gmail.com>
2024-08-31 21:58:54 +0000

    creacion de conexiones iniciales de filesystem

---
+++ filesystem/src/conexion.c
@@ -0,0 +1,23 @@
+#include "../include/conexion.h"
+
+int recv_handshake_filesystem(t_log *log, int server_fd)
+{
+    size_t bytes;
+
+    int module;
+    int32_t resultOk = 1;
+    int32_t resultError = -1;
+    bytes = recv(server_fd, &module, sizeof(int), MSG_WAITALL);
+    if (module == MEMORIA)
+    {
+        bytes = send(server_fd, &resultOk, sizeof(int32_t), 0);
+        log_info(log, "Buenas tardes, caballero.");
+        return module;
+    }
+    else
+    {
+        bytes = send(server_fd, &resultError, sizeof(int32_t), 0);
+        log_info(log, "Che, se quiso meter un desconocido.");
+        return -1;
+    }
+}
--- filesystem/src/main.c
+++ filesystem/src/main.c
@@ -1,10 +1,32 @@
-#include <../include/hello.h>
-#include <../include/server.h>
+#include "../include/main.h"
 
 int main(int argc, char *argv[])
 {
-    levantar_config(getcwd(NULL, 0), "filesystem");
+    t_config *config = NULL;
+    t_log *log = NULL;
+    int server_fd = 0;
+    int client_fd = 0;
 
-    saludar("filesystem");
-    return 0;
+    config = levantar_config(getcwd(NULL, 0), "filesystem");
+    log = levantar_log(getcwd(NULL, 0), "filesystem", config_get_string_value(config, "LOG_LEVEL"));
+
+    // fileSystem como sv
+    server_fd = iniciar_servidor(log, config_get_string_value(config, "PUERTO_ESCUCHA"));
+
+    while ((client_fd = esperar_cliente(log, server_fd)) != -1)
+    {
+        // Nuevo socket de conexión para cada nuevo cliente
+        int *client_connection = malloc(sizeof(client_fd));
+        *client_connection = client_fd;
+        
+        if(recv_handshake_filesystem(log,*client_connection) == MEMORIA){
+            atender_cliente(log, client_connection, atenderMemoria);
+        }else{
+            log_info(log, "Alguien no deseado quizo entrar");
+            free(client_connection);
+            close(client_fd);
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
